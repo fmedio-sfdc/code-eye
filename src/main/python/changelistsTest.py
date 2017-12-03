454345872843616322dcb8587e64b19953694b06
Seth <stager@salesforce.com>
2017-12-03 10:32:55 -0800

    skip cls by wcheung; fix bug parsing cl comment; fix duplicate filenames

--- src/main/python/changelistsTest.py
+++ src/main/python/changelistsTest.py
@@ -28,6 +28,16 @@ Affected files ...
 ... //app/main/core/chatter/java/src/core/userprofile/actions/OutOfOfficeAction.java#1 add"""
         self.assertEqual(changelists.parseFiles(badGusId), [])
 
+    def test_file_parsing(self):
+        cl = """Change 13149790 by s.chaturvedi@gridmanager:vm:10.252.18.250 on 2017/03/09 23:59:14
+    https://gus.my.salesforce.com/a07B00000030Eta
+Affected files ...
+... //app/f1.java#1 edit
+... //app/f2.java#2 edit"""
+        files = changelists.parseFiles(cl)
+        self.assertEqual(len(files), 2)
+        self.assertEqual([f['filename'] for f in files], ['//app/f1.java#1', '//app/f2.java#2'])
+
 if __name__ == '__main__':
     unittest.main()
 
